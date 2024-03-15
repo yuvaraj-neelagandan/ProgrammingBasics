14b8f83aa82e0dab502787c3dc4f89f50456c275
YuvarajNeelagandan <yuvaraj1402@gmail.com>
2024-03-15 12:32:18 +0530

    Initial commit
    Programming Basics Initial Commit

---
+++ basics.js
@@ -0,0 +1,100 @@
+console.log("Hello world")
+
+//data types
+
+//numbers
+let a = 5;
+let b = 5.5;
+
+//boolean
+let flag = true;
+let flagT = false;
+
+//char
+let char = 'c';
+
+//string
+let stringData = 'Nalan Academy';
+
+//arrays
+
+let march = [1, 2, 3, 4, 5];
+
+// console.log(march);
+// console.log(march[4]);
+
+// const c = 5;
+// a = 6;
+// console.log(c);
+
+// console.log(a);
+// console.log(b);
+
+// console.log(flag);
+// console.log(flagT);
+
+// console.log(char);
+// console.log(stringData);
+
+
+variable1 = 4;
+variable2 = 3;
+
+// result = additionFunction(variable1, variable2);
+// console.log(result);
+
+// result = subractionFunction(variable1, variable2);
+// console.log(result);
+
+// //functions
+// function additionFunction(a, b){
+//     return a+b
+// }
+
+// function subractionFunction(a, b){
+//     return a - b
+// }
+
+
+// //function with arguement with return type
+// result = additionFunction(variable1, variable2);
+// console.log(result);
+// function additionFunction(a, b){
+//     return a+b
+// }
+
+// //function with arguement without return type
+// result = additionFunction(variable1, variable2);
+// console.log(result);
+
+// function additionFunction(a, b){
+//     result = a+b
+//     console.log(result);
+// }
+
+//function without arguement without return type
+// result = additionFunction();
+// console.log(result);
+
+// function additionFunction(){
+//     result = variable1 + variable2
+//     console.log(result);
+// }
+
+
+//function without argument with return type
+result = additionFunction();
+console.log(result);
+
+function additionFunction(){
+    result = variable1 + variable2
+    return result;
+}
+
+
+
+
+//parameter
+
+
+//return type
